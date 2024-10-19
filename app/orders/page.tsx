026b116292fbd434298724e7d27ea83a29c3ae20
manandakrishna <manandakrishna@gmail.com>
2024-10-20 00:53:06 +0800

    Base version with MUI, toolpad, basic navigatio

---
+++ app/orders/page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+// Test
+export default function OrdersPage() {
+  return <Typography>Welcome to the Toolpad orders (orders page)!</Typography>;
+}
